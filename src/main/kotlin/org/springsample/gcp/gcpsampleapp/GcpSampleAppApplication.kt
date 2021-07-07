d59d98fc497599c70e80d51cdd9831621103a2de
nikodem911 <nikodem.jura@gmail.com>
2021-07-07 13:19:19 +0200

    Add storing basic entity in GCP Cloud Store

--- src/main/kotlin/org/springsample/gcp/gcpsampleapp/GcpSampleAppApplication.kt
+++ src/main/kotlin/org/springsample/gcp/gcpsampleapp/GcpSampleAppApplication.kt
@@ -1,11 +1,42 @@
 package org.springsample.gcp.gcpsampleapp
 
+import com.google.cloud.spring.data.datastore.core.mapping.Entity
+import com.google.cloud.spring.data.datastore.repository.DatastoreRepository
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class GcpSampleAppApplication
 
 fun main(args: Array<String>) {
-	runApplication<GcpSampleAppApplication>(*args)
+    runApplication<GcpSampleAppApplication>(*args)
 }
+
+@RestController
+class HelloController(
+        private val personRepository: PersonRepository
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @GetMapping("/hello")
+    fun hello(name: String): String {
+        log.info("Hello: ${name}")
+        personRepository.save(Person(name = name))
+        return "Hello ${name}"
+    }
+}
+
+@RepositoryRestResource
+interface PersonRepository : DatastoreRepository<Person, Long>
+
+@Entity
+data class Person(
+        @Id
+        var id: Long? = null,
+        var name: String
+)
